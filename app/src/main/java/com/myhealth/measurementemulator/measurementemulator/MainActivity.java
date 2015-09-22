fdb246f2b8c707e9a5b91d056755e54dae95c875
MasterFenrir <masterfenrir@outlook.com>
2015-09-22 17:10:31 +0200

    Added some very basic bluetooth functionality.

--- app/src/main/java/com/myhealth/measurementemulator/measurementemulator/MainActivity.java
+++ app/src/main/java/com/myhealth/measurementemulator/measurementemulator/MainActivity.java
@@ -1,11 +1,24 @@
 package com.myhealth.measurementemulator.measurementemulator;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView text = (TextView) findViewById(R.id.mainText);
+        text.setText("Button pressed");
+        new Thread(new BlueToothConnector()).start();
     }
 
     @Override
@@ -33,5 +52,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void discoverBlueTooth(View view) {
+        Log.d(TAG, "Button pressed");
+        TextView text = (TextView) findViewById(R.id.mainText);
+        Log.d(TAG, "Text changed");
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        Log.d(TAG, "Intent created");
+        startActivityForResult(discoverableIntent, 1);
     }
 }
