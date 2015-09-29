bc17878d1f6521f8d534879707cccabf4c55d1a7
Wessel Becker <wesselbecker92@gmail.com>
2015-09-29 11:57:06 +0200

    structure

---
+++ app/src/test/java/com/myhealth/measurementemulator/measurementemulator/main/test2.java
@@ -0,0 +1,7 @@
+package com.myhealth.measurementemulator.measurementemulator.main;
+
+/**
+ * Created by wessel on 29/09/2015.
+ */
+public class test2 {
+}
---
+++ app/src/test/java/com/myhealth/measurementemulator/measurementemulator/measurement/test.java
@@ -0,0 +1,7 @@
+package com.myhealth.measurementemulator.measurementemulator.measurement;
+
+/**
+ * Created by wessel on 29/09/2015.
+ */
+public class test {
+}
